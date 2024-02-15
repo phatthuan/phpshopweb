2f8ff4646572c15494ad2abae86b502dc6d87a05
NguyenThuanPhat <52000699@student.tdtu.edu.vn>
2024-02-15 19:44:12 +0700

    Update login

--- app/Http/Controllers/Admin/Users/LoginController.php
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
 {
@@ -14,6 +16,20 @@ class LoginController extends Controller
     }
 
     public function store(Request $request){
-        dd($request->input());
+        $this->validate($request,[
+            'email' => 'required|email:filter',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt([
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ], $request->input('remember'))){
+
+            return redirect()->route('admin');
+        }
+
+        Session::flash('error', 'Email hoặc Password không đúng');
+        return redirect()->back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('admin/users/login',[LoginController::class, 'index']);
+Route::get('admin/users/login',[LoginController::class, 'index'])->name('login');
 Route::post('admin/users/login/store', [LoginController::class, 'store']);
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('admin/main', [MainController::class, 'index'])->name('admin');
+});
